356446d3bcdf7ac05d135680fbbb7cd336b58701
Matias Meza <topo@firstjob.cl>
2023-12-20 19:56:56 +0000

    Merged in feature/CRM-137-plataforma-de-datos-slide-2 (pull request #3)
    Feature/CRM-137 plataforma de datos slide 2
    
    * [add] se crean metodos en concern
    
    * [add] Se crea partial para chart js y ccs necesario
    
    * [add] Se añade rails env production en linea de dockerfile
    
    * [fix] se arregla typo para renderear posicion de TOM
    
    * [add] Se agrega logos blancos de studios para login, y se eliminan los mailers de status

--- app/controllers/dashboards_admin_controller.rb
+++ app/controllers/dashboards_admin_controller.rb
@@ -2,6 +2,8 @@ class DashboardsAdminController < ApplicationController
   before_action :authenticate_admin!, except: [:initialize]
   before_action :handle_index, only: [:index]
   
+  include DashboardConcern
+
   def initialize
     super
     # Include the main layout bootstrap file
@@ -29,34 +31,8 @@ class DashboardsAdminController < ApplicationController
     get_study_information # Establece variables de instancia para la información base de los estudios
     
     get_general_information # Establece las variables de instacia para la información de los rankings generales del cliente
-  end
 
-  def get_client_options
-    clients = Client.select("clients.id, clients.name || ' | ' || countries.iso as label").joins(:country).left_joins(:client_general_results, :client_historical_results).where("client_general_results.is_active = true OR client_historical_results.is_active = true").order('label')
-
-    clients.empty? ? [] : clients.order('label ASC').map { |option| [option.label, option.id] }
-  end
-
-  def get_client_by_id(id)
-    Client.find_by(id: id)
-  end
-
-  def get_study_information
-    @efy = get_active_study_by_key_name('efy')
-    @bie = get_active_study_by_key_name('bie')
-    @tom = get_active_study_by_key_name('tom')
-  end
-
-  def get_general_information
-    client_general_result = @client&.client_general_results&.where(client_general_results: { is_active: true })&.select(:results)&.first
-
-    @efy_general = client_general_result&.results&.dig('efy')
-    @bie_general = client_general_result&.results&.dig('bie')
-    @tom_general = client_general_result&.results&.dig('tom')
-  end
-
-  def get_active_study_by_key_name(key_name)
-    Study.find_by(key_name: key_name, is_active: true)
+    get_historical_results
   end
 
 end
--- app/controllers/dashboards_user_controller.rb
+++ app/controllers/dashboards_user_controller.rb
@@ -2,6 +2,8 @@ class DashboardsUserController < ApplicationController
   before_action :authenticate_user!, except: [:initialize]
   before_action :handle_index, only: [:index]
   
+  include DashboardConcern
+  
   def initialize
     super
     # Include the main layout bootstrap file
@@ -31,6 +33,8 @@ class DashboardsUserController < ApplicationController
     get_study_information # Establece variables de instancia para la información base de los estudios
     
     get_general_information # Establece las variables de instacia para la información de los rankings generales del cliente
+ 
+    get_historical_results
   end
 
   def handle_user_accesses
@@ -44,36 +48,6 @@ class DashboardsUserController < ApplicationController
 
   def get_user_access_client_ids(id)
     UserAccess.where(user_id: id).pluck(:client_id)
-  end
-
-  def get_client_options(ids = [])
-    clients = Client.select("clients.id, clients.name || ' | ' || countries.iso as label").joins(:country).left_joins(:client_general_results, :client_historical_results).where("client_general_results.is_active = true OR client_historical_results.is_active = true").order('label')
-
-    clients = clients.where(id: ids) unless ids.empty?
-
-    clients.empty? ? [] : clients.order('label ASC').map { |option| [option.label, option.id] }
-  end
-
-  def get_client_by_id(id)
-    Client.find_by(id: id)
-  end
-
-  def get_study_information
-    @efy = get_active_study_by_key_name('efy')
-    @bie = get_active_study_by_key_name('bie')
-    @tom = get_active_study_by_key_name('tom')
-  end
-
-  def get_general_information
-    client_general_result = @client&.client_general_results&.where(client_general_results: { is_active: true })&.select(:results)&.first
-
-    @efy_general = client_general_result&.results&.dig('efy')
-    @bie_general = client_general_result&.results&.dig('bie')
-    @tom_general = client_general_result&.results&.dig('tom')
-  end
-
-  def get_active_study_by_key_name(key_name)
-    Study.find_by(key_name: key_name, is_active: true)
   end
 
 end
